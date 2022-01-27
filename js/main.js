d158c7450f45593f2195941a2c0c33d334305afe
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2022-01-27 16:03:52 +0300

    Day 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 "use strict";
+
+// Swiper slider
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 2.35,
+  loop: true,
+  speed: 400,
+  spaceBetween: 70,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  toggle: true,
+  effect: "coverflow",
+  grabCursor: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 150,
+    modifier: 1,
+    slideShadows: true
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.35
+    },
+    320: {
+      effect: 'slide',
+      direction: 'vertical',
+      loop: false,
+      slidesPerView: 3
+    }
+  }
+}); // --- end of Swiper slider ---
