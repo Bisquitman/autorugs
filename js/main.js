ff47e1e4db3544a7a6ab15d2710bc29a8936a9a1
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2022-01-28 17:01:51 +0300

    Day 4

--- js/main.js
+++ js/main.js
@@ -9,24 +9,60 @@ var swiper = new Swiper('.swiper', {
   centeredSlides: true,
   slideToClickedSlide: true,
   toggle: true,
-  effect: "coverflow",
+  // effect: "coverflow",
   grabCursor: true,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 150,
-    modifier: 1,
-    slideShadows: true
-  },
+  // coverflowEffect: {
+  //   rotate: 0,
+  //   stretch: 0,
+  //   depth: 150,
+  //   modifier: 1,
+  //   slideShadows: true,
+  // },
   breakpoints: {
     768: {
-      slidesPerView: 2.35
+      slidesPerView: 2.35,
+      loop: true
     },
     320: {
-      effect: 'slide',
+      // effect: 'slide',
       direction: 'vertical',
       loop: false,
       slidesPerView: 3
     }
   }
 }); // --- end of Swiper slider ---
+// Модалка
+
+var modalWindow = document.querySelector('.overlay');
+var orderBtn = document.querySelector('.main-display__button');
+var sendBtn = document.querySelector('.modal__button');
+
+var disableScroll = function disableScroll() {
+  var scrollWidth = window.innerWidth - document.body.offsetWidth;
+  document.body.scrollPosition = window.scrollY;
+  document.documentElement.style.cssText = "position: relative; height: 100vh;";
+  document.body.style.cssText = "\n    overflow: hidden;\n    position: fixed;\n    top: -".concat(document.body.scrollPosition, "px;\n    left: 0;\n    height: 100vh;\n    width: 100vw;\n    padding-right: ").concat(scrollWidth, "px;\n  ");
+};
+
+var enableScroll = function enableScroll() {
+  document.documentElement.style.cssText = '';
+  document.body.style.cssText = "position: relative;";
+  window.scroll({
+    top: document.body.scrollPosition
+  });
+};
+
+orderBtn.addEventListener('click', function () {
+  disableScroll();
+  modalWindow.classList.add('show_modal');
+});
+modalWindow.addEventListener('click', function (event) {
+  if (event.target.classList.contains('overlay')) {
+    enableScroll();
+    modalWindow.classList.remove('show_modal');
+  }
+});
+sendBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  modalWindow.classList.remove('show_modal');
+}); // --- end of Модалка ---
